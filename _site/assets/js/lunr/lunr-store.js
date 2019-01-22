ab71df9cff611d62403ca07c35f0eea7a843a33a
Jongwoo Han <han980817@gmail.com>
2019-01-22 23:20:35 +0900

    MJU(Seoul) x Likelion Homepage

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,10 @@ var store = [{
         "title": "Test",
         "excerpt":"테스트용 포스트입니다.  ","categories": ["Test"],
         "tags": [],
-        "url": "http://localhost:4000/test/welcome-to-jekyll/",
+        "url": "http://localhost:4000/test/test/",
+        "teaser":null},{
+        "title": "Test2",
+        "excerpt":"테스트용 포스트입니다.   테스트용 포스트입니다.  ","categories": ["Test"],
+        "tags": [],
+        "url": "http://localhost:4000/test/test2/",
         "teaser":null}]
