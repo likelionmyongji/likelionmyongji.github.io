9d5b6a9838eccf08df25a6f4bcd341aad093fd27
Junghoon <bjh5230@gmail.com>
2019-01-23 11:56:50 +0900

    MJU(Seoul) x Likelion Homepage

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
         "title": "블로그 생성",
         "excerpt":"테스트용 포스트입니다.  ","categories": ["blogging"],
-        "tags": [],
+        "tags": ["blog","daily","posting"],
         "url": "http://localhost:4000/blogging/test/",
         "teaser":null},{
         "title": "안녕하세요",
         "excerpt":"테스트용 포스트입니다.   테스트용 포스트입니다.  ","categories": ["note"],
-        "tags": [],
+        "tags": ["note","test","blog","posting"],
         "url": "http://localhost:4000/note/test2/",
         "teaser":null}]
