2c839eac63721f9b66d67837988def41d5011f85
Junghoon <bjh5230@gmail.com>
2019-01-23 20:26:45 +0900

    MJU(Seoul) x Likelion Homepage

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "블로그 생성",
-        "excerpt":"테스트용 포스트입니다.  ","categories": ["blogging"],
+        "excerpt":"테스트용 포스트입니다.   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  ","categories": ["blogging"],
         "tags": ["blog","daily","posting"],
         "url": "http://localhost:4000/blogging/test/",
         "teaser":null},{
