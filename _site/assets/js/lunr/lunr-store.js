6b7e2398123e4a13f52540396d0f033f454c71f4
Jongwoo <han980817@gmail.com>
2019-02-04 23:41:02 +0900

    MJU(Seoul) x Likelion Homepage

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,14 +1,4 @@
 var store = [{
-        "title": "Lorem Ipsum",
-        "excerpt":"테스트용 포스트입니다.   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.   Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of “de Finibus Bonorum et Malorum” (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, “Lorem ipsum dolor sit amet..”, comes from a line in section 1.10.32.  ","categories": ["blogging"],
-        "tags": ["blog","daily","posting"],
-        "url": "http://localhost:4000/blogging/test/",
-        "teaser":null},{
-        "title": "안녕하세요",
-        "excerpt":"테스트용 포스트입니다.   테스트용 포스트입니다.  ","categories": ["note"],
-        "tags": ["note","test","blog","posting"],
-        "url": "http://localhost:4000/note/test2/",
-        "teaser":null},{
         "title": "7기 모집 안내",
         "excerpt":"2019년도 명지대학교(서울) 멋쟁이 사자처럼의 세부 선발 계획에 대해 공지 드립니다.                        ","categories": ["post"],
         "tags": ["recruit"],
