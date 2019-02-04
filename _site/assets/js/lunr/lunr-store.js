a03c7efe8a35d38a911f025485759128d0745ad8
Jongwoo <han980817@gmail.com>
2019-02-04 23:59:43 +0900

    MJU(Seoul) x Likelion Homepage

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "7기 모집 안내",
-        "excerpt":"2019년도 명지대학교(서울) 멋쟁이 사자처럼의 세부 선발 계획에 대해 공지 드립니다.                        ","categories": ["post"],
+        "excerpt":"2019년도 명지대학교(서울) 멋쟁이 사자처럼의 선발 계획에 대해 공지 드립니다.                        ","categories": ["post"],
         "tags": ["recruit"],
         "url": "http://localhost:4000/post/recruit/",
         "teaser":null}]
