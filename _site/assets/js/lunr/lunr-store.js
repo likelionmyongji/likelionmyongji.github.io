2465291a1ded468729f9711d75cd4ac7d9feb627
Jongwoo Han <han980817@gmail.com>
2019-07-03 16:46:26 +0900

    MJU(Seoul) x Likelion Homepage

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "자바스크립트 입문 제 0강",
+        "excerpt":"자바스크립트란?   JavaScripts는 웹 브라우저에서 사용하기 위해 만들어진 프로그래밍 언어입니다. 이 언어는 웹 브라우저 상에서 나타나는 UI(사용자 인터페이스)를 동적으로 보여주기 위해 사용됩니다. 최근에는 브라우저 뿐만 아니라 NodeJS 런타임을 통해 서버 쪽에서도 사용되고 있습니다.   또한,  NW.js, Electron 등을 통해 데스크탑 앱을 만들 수도 있고, react-native, NativeScript 등으로 모바일 앱도 만들 수 있습니다.   JavaScript는 매년마다 새로운 문법들이 나오며 나날히 발전해 가고 있습니다. 명지대학교(서울) 멋쟁이사자처럼에서는 2학기 교육과정으로 React가 결졍됨에 따라 여름 방학 동안 7기 아기사자들이 쉽게 JavaScript를 이해할 수 있도록 교안을 정리해 올려보고자 합니다.  ","categories": ["JavaScript"],
+        "tags": ["JavaScript","vanillaJS","es6","lecture"],
+        "url": "http://localhost:4000/javascript/js_lecture0/",
+        "teaser":null},{
+        "title": "자바스크립트 입문 제 1강",
+        "excerpt":"Hello JavaScript!   자바스크립트는 브라우저에서 언제든지 사용해 볼 수 있습니다. 만약 크롬브라우저가 아닌 다른 브라우저를 사용중이라면 크롬을 설치하여 실행해보세요. 그리고 개발자 도구를 열어보세요.   개발자 도구는      윈도우 Ctrl + Shift + I   macOS Command + Option + I   키를 눌러서 열 수 있습니다.     탭에서 Console을 선택하고 다음 코드를 입력창에 입력해보세요.  console.log('HelloLion!');     여기서 console.log는 Console에 특정 내용을 출력하는 명령입니다.     다음으로 아래의 코드를 입력해볼까요?   console.log(1 + 2);   Console에 3이 출력됨에 따라 우리는 JavaScript로 연산을 할 수 있다는 것을 알 수 있습니다.     이제, 여러분이 원하는 내용을 한 번 출력해보세요.   console.log('명지대학교(서울) 멋쟁이사자처럼 7기 화이팅');     이번 교안의 내용은 여기까지입니다.   다음 교안에서는 JavaScript의 변수에 대해 알아보겠습니다.  ","categories": ["JavaScript"],
+        "tags": ["JavaScript","vanillaJS","es6","lecture"],
+        "url": "http://localhost:4000/javascript/js_lecture1/",
+        "teaser":null}]
